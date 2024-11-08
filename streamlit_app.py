3adb4ea781bfd7cbd6c02097f207a161f92d4840
Sarada01 <rachakonda_j@yahoo.co.uk>
2024-11-08 11:32:58 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,5 +8,17 @@ st.write(
     """Choose the frits you want in your customer Smoothie!
     """
 )
+
+name_on_order = st.text_input("Name on the Smoothie:")
+st.write(name_on_order)
+
 cnx = st.connection("snowflake")
 session = cnx.session()
+
+my_dataframe = session.table("smoothies.public.fruit_options") \
+    .select(col("FRUIT_NAME"))
+
+ingradient_list = st.multiselect("Choose upto 5 Ingredients:"
+    , my_dataframe
+    , max_selections = 5
+    )
